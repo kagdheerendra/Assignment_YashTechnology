1fe0a1fb552526718da3c05faf849fc9c5035b11
Dheerendra  Kag <dheerendra.kag@Yash.com>
2022-09-30 12:45:16 +0530

    fix: test case commit

--- javaTraining/src/com/yash/assignment/java8/StudentStreamTask3.java
+++ javaTraining/src/com/yash/assignment/java8/StudentStreamTask3.java
@@ -3,6 +3,8 @@ package com.yash.assignment.java8;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StudentStreamTask3 {
 
@@ -11,8 +13,12 @@ public class StudentStreamTask3 {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         list.add(new Student(101, LocalDateTime.parse("2016-03-04 11:30", formatter), LocalDateTime.parse("1994-03-04 11:30", formatter), "40", "45", "50", "55", "60", "first"));
         list.add(new Student(101, LocalDateTime.parse("2017-03-04 11:30", formatter), LocalDateTime.parse("1995-03-04 11:30", formatter), "60", "55", "50", "45", "40", "second"));
+        //double sum = list.stream().mapToDouble(Student::Double.parseDouble(getEnglish())).sum();
         DoubleSummaryStatistics stats = list.stream().mapToDouble(s->Double.parseDouble(s.getEnglish())+Double.parseDouble(s.getChemistry())+Double.parseDouble(s.getHindi())+Double.parseDouble(s.getPhysics())+Double.parseDouble(s.getMaths())).summaryStatistics();
 	    System.out.println(stats);
+	    
+	    Map<String, List<Student>> collect = list.stream().collect(Collectors.groupingBy(Student::getEnglish));
+	    System.out.println(collect.toString());
 	}
 
 }
